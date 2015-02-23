cf6a6720a24ff6901515cb3e9cf315934a330cef
William Wolf <throughnothing@gmail.com>
2015-02-23 02:43:32 -0800

    Initial Tests (Wallet)

---
+++ spec/walletspec.js
@@ -0,0 +1,30 @@
+'use strict';
+var Wallet = require('../').Wallet;
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
+
+describe('Wallet', function() {
+
+  describe('Constructor', function() {
+
+    it('instantiates with no args',function () {
+      var w = new Wallet();
+    });
+
+  });
+
+  describe('#toJSON method', function() {
+
+    it('#toJson',function () {
+      var w = new Wallet();
+      var json = w.toJSON();
+      json.version.should.equal('1');
+      json.type.should.equal('wallet');
+      expect(json.pub.should.be.a('string'));
+      expect(json.priv.should.be.a('string'));
+    });
+
+  });
+});
